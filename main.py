49bef8bb9cb25b42b27ffd6fca5be85efe2ed970
james599 <80703170+james599@users.noreply.github.com>
2023-09-17 21:07:18 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import mido
+import mido.backends.rtmidi
+import requests
+import datetime
+
+#Lower Thirds ON Trigger, Lower Thirds OFF Trigger, Slides, Main, Left, Split Screen, YT presenter ON, YT presenter OFF
+#Lower Thirds Forced OFF
+MIDILoc = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+CompLoc = ["1/1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7", "1/8", "2/1", "2/2", "2/3", "2/4", "2/5", "2/6", "2/7", "2/8"]
+
+def Log(e):
+    with open("ProclaimAutomationLog.txt", "a") as f:
+        f.write(datetime.now() + e)
+        return e
+
+def CompanionTrigger(MIDI):
+    try:
+        requests.get("http://127.0.0.1:8000/press/bank/" + CompLoc[MIDILoc.index(MIDI)])
+    except Exception as e:
+        print("Change the Companion interface to be running on 127.0.0.1:8000\n" + Log(e))
+
+def main(LowerThirds):
+    while True:
+        while inport.closed:
+            print(Log("LoopMIDI Not running 'ProclaimCompanion' correctly"))
+        msg = inport.receive()
+        MIDI = int(str(msg).split(" ")[2].split("=")[1])
+        if MIDI in MIDILoc:
+            if (MIDI == 1 and LowerThirds == True) or (MIDI == 2 and LowerThirds == False):
+                break
+            elif MIDI == 1:
+                LowerThirds = True
+                CompanionTrigger(MIDI)
+            elif MIDI == 2:
+                LowerThirds = False
+                CompanionTrigger(MIDI)
+            else:
+                CompanionTrigger(MIDI)
+
+def Initialise():
+    CompanionTrigger(9)
+    main(LowerThirds = False)
+
+if __name__ == "__main__":
+    print("Just close the window to stop automation...")
+
+    try:
+        inport = mido.open_input("ProclaimCompanion 0")
+        print(Log("Running..."))
+        connected = True
+    except Exception as e:
+        print("LoopMIDI Not running 'ProclaimCompanion' correctly\nPlease re-run program\n" + Log(e))
+        connected = False
+
+    if connected:
+        Initialise()
