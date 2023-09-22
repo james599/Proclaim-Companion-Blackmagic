2e726f69ec4547575b03a4ce63b96788ff395bd3
james599 <80703170+james599@users.noreply.github.com>
2023-09-22 20:30:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import mido
 import mido.backends.rtmidi
 import requests
-import datetime
+from datetime import datetime
+import time
 
 #Lower Thirds ON Trigger, Lower Thirds OFF Trigger, Slides, Main, Left, Split Screen, YT presenter ON, YT presenter OFF
-#Lower Thirds Forced OFF
+#Lower Thirds forced OFF
 MIDILoc = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 CompLoc = ["1/1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7", "1/8", "2/1", "2/2", "2/3", "2/4", "2/5", "2/6", "2/7", "2/8"]
 
 def Log(e):
     with open("ProclaimAutomationLog.txt", "a") as f:
-        f.write(datetime.now() + e)
-        return e
+        f.write(str(datetime.now()) + str(e))
+        return str(e)
 
 def CompanionTrigger(MIDI):
     try:
         requests.get("http://127.0.0.1:8000/press/bank/" + CompLoc[MIDILoc.index(MIDI)])
     except Exception as e:
         print("Change the Companion interface to be running on 127.0.0.1:8000\n" + Log(e))
+        time.sleep(20)
 
 def main(LowerThirds):
     while True:
@@ -26,7 +28,7 @@ def main(LowerThirds):
         msg = inport.receive()
         MIDI = int(str(msg).split(" ")[2].split("=")[1])
         if MIDI in MIDILoc:
-            if (MIDI == 1 and LowerThirds == True) or (MIDI == 2 and LowerThirds == False):
+            if (MIDI == 1 and LowerThirds is True) or (MIDI == 2 and LowerThirds is False):
                 break
             elif MIDI == 1:
                 LowerThirds = True
@@ -37,10 +39,6 @@ def main(LowerThirds):
             else:
                 CompanionTrigger(MIDI)
 
-def Initialise():
-    CompanionTrigger(9)
-    main(LowerThirds = False)
-
 if __name__ == "__main__":
     print("Just close the window to stop automation...")
 
@@ -50,7 +48,9 @@ if __name__ == "__main__":
         connected = True
     except Exception as e:
         print("LoopMIDI Not running 'ProclaimCompanion' correctly\nPlease re-run program\n" + Log(e))
+        time.sleep(20)
         connected = False
 
     if connected:
-        Initialise()
+        CompanionTrigger(9)
+        main(LowerThirds = False)
