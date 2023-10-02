e282a096c10d0200e8547e8add008ef28fd93023
james599 <80703170+james599@users.noreply.github.com>
2023-10-02 15:32:35 +0100

    Update main.py
    update MIDILoc to use indexing

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 
 #Lower Thirds ON Trigger, Lower Thirds OFF Trigger, Slides, Main, Left, Split Screen, YT presenter ON, YT presenter OFF
 #Lower Thirds forced OFF
-MIDILoc = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 CompLoc = ["1/1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7", "1/8", "2/1", "2/2", "2/3", "2/4", "2/5", "2/6", "2/7", "2/8"]
 
 def Log(e):
@@ -16,7 +15,7 @@ def Log(e):
 
 def CompanionTrigger(MIDI):
     try:
-        requests.get("http://127.0.0.1:8000/press/bank/" + CompLoc[MIDILoc.index(MIDI)])
+        requests.get("http://127.0.0.1:8000/press/bank/" + CompLoc[MIDI-1])
     except Exception as e:
         print("Change the Companion interface to be running on 127.0.0.1:8000\n" + Log(e))
         time.sleep(20)
